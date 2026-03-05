3045263d347f89a67df0b9206a67a5e309382221
pvmeht <pvmehta9511pm@gmail.com>
2026-03-06 01:09:53 +0530

    added websocket to backend

---
+++ Backend/src/main/java/com/labourhand/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.labourhand.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Use /topic to broadcast out to clients
+        config.enableSimpleBroker("/topic");
+        // Clients send messages to /app which are routed to @MessageMapping methods
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // The endpoint the client connects to: ws://localhost:8081/ws
+        registry.addEndpoint("/ws")
+                // Need to allow CORS since React is on port 5173
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
