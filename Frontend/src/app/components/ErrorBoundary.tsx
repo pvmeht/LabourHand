a16084aa65b3410e45b202f640fddef6885561e6
pvmeht <pvmehta9511pm@gmail.com>
2026-03-08 21:11:02 +0530

    implemeneted error pages

--- Frontend/src/app/components/ErrorBoundary.tsx
+++ Frontend/src/app/components/ErrorBoundary.tsx
@@ -15,6 +15,7 @@ export class ErrorBoundary extends Component<Props, State> {
   public state: State = {
     hasError: false
   };
+  
 
   public static getDerivedStateFromError(error: Error): State {
     // Update state so the next render will show the fallback UI.
