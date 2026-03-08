098056618ba42a59270d6bd2e453984f932cac2f
pvmeht <pvmehta9511pm@gmail.com>
2026-03-08 21:06:49 +0530

    added solution for CORS

--- Backend/src/main/java/com/labourhand/config/WebSocketConfig.java
+++ Backend/src/main/java/com/labourhand/config/WebSocketConfig.java
@@ -22,8 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // The endpoint the client connects to: ws://localhost:8081/ws
         registry.addEndpoint("/ws")
-                // Need to allow CORS since React is on port 5173
-                .setAllowedOriginPatterns("*")
+                // Need to allow CORS since React is on port 5173/5174
+                .setAllowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:3000")
                 .withSockJS();
     }
 }
